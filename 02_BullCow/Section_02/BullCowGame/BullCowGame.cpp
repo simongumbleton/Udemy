2532a4980aacd8229a2f44f69de1d940f67fc4a3
simongumbleton <simongumbleton@gmail.com>
2017-11-26 12:26:59 +0000

    Removing test comments

--- 02_BullCow/Section_02/BullCowGame/BullCowGame.cpp
+++ 02_BullCow/Section_02/BullCowGame/BullCowGame.cpp
@@ -1,13 +1,9 @@
 // BullCowGame.cpp : Defines the entry point for the console application.
-//
-//Testing Git source control
 
 #include "stdafx.h"
 
 
 int main()
 {
-    //Test
-    //Test2
     return 0;
 }
