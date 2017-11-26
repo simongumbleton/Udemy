d42f018e73cc8a43d60cb6a086399b00fa6c6f86
simongumbleton <simongumbleton@gmail.com>
2017-11-26 17:06:08 +0000

    Latest working

--- 02_BullCow/Section_02/BullCowGame/main.cpp
+++ 02_BullCow/Section_02/BullCowGame/main.cpp
@@ -4,25 +4,23 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrint();
+
+//Entry point for the game
 int main() {
 
-	//Introduce the game
-	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game!\n";
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 
-	//Get a guess from the user
-	cout << "Make a guess...";
-	string Guess = "";
-	getline(cin, Guess);	//std::getline (std::cin,name);
-	//Repeat the gues back to the user
-	cout << "\nYou guessed.." << Guess << endl;
+	PrintIntro();
 
-	//Get a guess from the user
-	cout << "Make a guess...";
-	getline(cin, Guess);	//std::getline (std::cin,name);
-	//Repeat the gues back to the user
-	cout << "\nYou guessed.." << Guess;
+
+	constexpr int NUMBER_OF_TURNS = 5;	// constant expression defines the number of turns
+	//Loop for the number of turns asking for guesses
+	for (int i = 1; i <= NUMBER_OF_TURNS; i++)
+	{
+		GetGuessAndPrint();
+		cout << endl;
+	}
 
 
 	//Evaluate Guess
@@ -30,4 +28,26 @@ int main() {
 
 	cout << endl;
 	return 0;
-};
+}
+
+void PrintIntro()
+{
+	//Introduce the game
+	constexpr int WORD_LENGTH = 5;	// constant expression defines the length of the word to guess
+	cout << "Welcome to Bulls and Cows, a fun word game!\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	return;
+}
+
+string GetGuessAndPrint()
+{
+	//Get a guess from the user
+	cout << "Make a guess...";
+	string Guess = "";
+	getline(cin, Guess);	//std::getline (std::cin,name);
+
+	//Repeat the gues back to the user
+	cout << "\nYou guessed.." << Guess << endl;
+
+	return Guess;
+}
