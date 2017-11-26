31d9727faf79e83a6c2bf32eb41d1c1616fb7d31
simongumbleton <simongumbleton@gmail.com>
2017-11-26 16:35:21 +0000

    latest working

--- 02_BullCow/Section_02/BullCowGame/main.cpp
+++ 02_BullCow/Section_02/BullCowGame/main.cpp
@@ -1,13 +1,33 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
+	//Introduce the game
 	constexpr int WORD_LENGTH = 5;
-
-	cout << "Welcome to Bulls and Cows, a fun word game!" << endl;
+	cout << "Welcome to Bulls and Cows, a fun word game!\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+
+	//Get a guess from the user
+	cout << "Make a guess...";
+	string Guess = "";
+	getline(cin, Guess);	//std::getline (std::cin,name);
+	//Repeat the gues back to the user
+	cout << "\nYou guessed.." << Guess << endl;
+
+	//Get a guess from the user
+	cout << "Make a guess...";
+	getline(cin, Guess);	//std::getline (std::cin,name);
+	//Repeat the gues back to the user
+	cout << "\nYou guessed.." << Guess;
+
+
+	//Evaluate Guess
+	cout << "\nLets see how you did!..";
+
+	cout << endl;
 	return 0;
 };
